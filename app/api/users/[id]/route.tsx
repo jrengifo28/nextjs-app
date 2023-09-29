87f9ce2e62644112d4897acfa4e96ee07098e739
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2023-09-29 12:23:02 -0500

    Validating Requests with Zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 interface Props {
   params: { id: number };
@@ -20,8 +21,9 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
   if (!params.id)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
