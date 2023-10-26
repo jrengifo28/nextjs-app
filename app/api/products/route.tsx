8ed70867476a72e1c461588c665d76a7ada8378e
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2023-10-25 23:50:53 -0500

    Change Validation

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -11,9 +11,9 @@ export async function GET(request: NextRequest) {
 // Function POST
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validatio = schema.safeParse(body);
-  if (!validatio.success)
-    return NextResponse.json(validatio.error.errors, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
   const newProduct = await prisma.product.create({
     data: {
